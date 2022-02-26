c69e28543d076ef1ae8fd7f32970eee389054c01
Yuta-kun <cosmoscandium@gmail.com>
2022-02-26 21:27:37 +0530

    Seperated main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # ticker=[x for x in input("Please insert space separated tickers:").split(" ")]
 # start=input("Insert startdate (in yyyyMMdd format):")
 # end=input("Insert enddate (in yyyyMMdd format):")
-from StockMarketAnalysis
-ticker = ['TCS.NS', 'MSFT']
-start = '20200101'
-end = '20200630'
-idf = source(ticker, start, end, 'yahoo')
-df = preprocessing(idf, ticker)
-nanhandler(df)
+from StockMarketAnalyzer import *
+
+def main():
+    ticker = ['TCS.NS', 'MSFT']
+    start = '20200101'
+    end = '20200630'
+    idf = source(ticker, start, end, 'yahoo')
+    df = preprocessing(idf, ticker)
+    nanhandler(df)
+
+if __name__=="__main__":
+    main()
